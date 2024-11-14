52dc5d88a16880f257ffb34d42f0a4549f7b7eeb
Edison Kardirsson <ed.kardirsson@hotmail.com>
2024-11-14 20:30:25 +0100

    Adding Layers model class

--- mnistWithTensorflow.py
+++ mnistWithTensorflow.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
+from tensorflow.python.keras.layers import Conv2D, Flatten, Dense
+from tensorflow.python.keras import Model
+
 
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -18,3 +21,22 @@ print(y_train[0])
 
 plt.imshow(x_test[0], cmap='gray')
 plt.show()
+
+print(y_test[0])
+
+
+class MNISTModel(Model):
+    def __init__(self):
+        super(MNISTModel, self).__init__()
+        self.conv1 = Conv2D(32, kernel_size=3, activation='relu')
+        self.flatten = Flatten()
+        self.dense1 = Dense(128, activation='relu')
+        self.dense2 = Dense(10, activation='softmax')
+
+        def call(self, x):
+            x1 = self.conv1(x)
+            x2 = self.flatten(x1)
+            x3 = self.dense1(x2)
+            return self.dense2(x3)
+
+
